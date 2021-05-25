6b62613ad1b6cf86fcce8010cf1ba2c4a962cbf4
Corey Wademan <67563215+Corey-Wademan@users.noreply.github.com>
2021-05-25 19:05:54 -0400

    Update sql.js

--- sql.js
+++ sql.js
@@ -1,3 +1,19 @@
 const sqlite = require('sqlite3');
 const db = new sqlite.Database('path_to_file');
 
+db.serialize(() => {
+  db.run('DROP TABLE IF EXISTS Strip');
+  db.run(`CREATE TABLE IF NOT EXISTS Strip
+    (
+      id INTEGER PRIMARY KEY,
+      head TEXT NOT NULL,
+      body TEXT NOT NULL,
+      background TEXT NOT NULL,
+      bubble_type TEXT NOT NULL,
+      bubble_text TEXT NOT NULL DEFAULT "",
+      caption TEXT NOT NULL DEFAULT ""
+    )`);
+});
+
+
+ 
