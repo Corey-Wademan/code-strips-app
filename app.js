fb2a8452bcc9cd3829d19d0cd81c654b7ff5d8b3
Corey Wademan <67563215+Corey-Wademan@users.noreply.github.com>
2021-05-25 21:40:11 -0400

    Update app.js

--- app.js
+++ app.js
@@ -17,20 +17,41 @@ app.get('/strips', (req, res) => {
     } else {
       res.send({strips: rows});
     }
-  })
+  });
 });
 
 const validateStrip = (req, res, next) => {
-  const stripToCreate = req.body.strip;
-  if (!stripeToCreate.head || !stripeToCreate.body || !stripeToCreate.background || !stripeToCreate.bubbleType) {
+  const newStrip = req.body.strip;
+  if (!newStrip.head || !newStrip.body || !newStrip.background || !newStrip.bubbleType) {
    return res.sendStatus(400)
   }
   next();
-}
+};
 
 app.post('/strips', validateStrip, (req, res) => {
   const newStrip = req.body.strip;
-})
+  db.run(`INSERT INTO Strip (head, body, bubble_type, background, bubble_text,
+    caption) VALUES ($head, $body, $bubbleType, $background, $bubbleText,
+    $caption)`,
+    {
+      $head: newStrip.head,
+      $body: newStrip.body,
+      $background: newStrip.background,
+      $bubbleType: newStrip.bubbleType,
+      $bubbleText: newStrip.bubbleText,
+      $caption: newStrip.caption 
+    }, function(err) {
+      if (err) {
+        return res.sendStatus(500);
+      }
+    db.get(`SELECT * FROM Strip WHERE id = ${this.lastID}`, (err, row) => {
+      if (!row) {
+        return res.sendStatus(500)
+      }
+      res.send({strip: row}).status(201)
+    });
+  });
+});
 
 
 app.listen(PORT);
