8f9f1fccebe54f6a5875421ab0e01dffba23c2d8
Corey Wademan <67563215+Corey-Wademan@users.noreply.github.com>
2021-05-25 20:54:45 -0400

    Added Get Route

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 const PORT = process.env.PORT || 4001;
 const bodyParser = require('body-parser');
+const sqlite = require('sqlite3');
+const db = new sqlite.Database(process.env.TEST_DATABASE || './db.sqlite');
 
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 
+app.get('/strips', (req, res) => {
+  db.all('SELECT * FROM Strip', (err, rows) => {
+    if (err) {
+      res.sendStatus(500);
+    } else {
+      res.send({strips: rows});
+    }
+  })
+});
 
+const validateStrip = (req, res, next) => {
+  const stripToCreate = req.body.strip;
+  if (!stripeToCreate.head || !stripeToCreate.body || !stripeToCreate.background || !stripeToCreate.bubbleType) {
+   return res.sendStatus(400)
+  }
+  next();
+}
+
+app.post('/strips', validateStrip, (req, res) => {
+  const newStrip = req.body.strip;
+})
 
 
 app.listen(PORT);
